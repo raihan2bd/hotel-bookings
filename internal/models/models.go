e6966b07b9c2a05bb868e6276cf230847df7e39a
Abu Raihan <raihan2bd.official@gmail.com>
2023-03-02 19:44:48 +0600

    Add mail data model to hold mail data

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"html/template"
+	"time"
+)
 
 // User is the user model
 type User struct {
@@ -58,4 +61,12 @@ type RoomRestriction struct {
 	Room          Room
 	Reservation   Reservation
 	Restriction   Restriction
+}
+
+// MailData hold an email message
+type MailData struct {
+	To      string
+	From    string
+	Subject string
+	Content template.HTML
 }
